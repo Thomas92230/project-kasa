3e3adc55ae658f3e4f7de03ec181d0e19b5910cd
Icham Boudehane <boudehaneicham@gmail.com>
2023-07-04 18:42:26 +0200

    kasa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import "./styles/index.scss"
+import Home from './pages/Home';
+import About from "./pages/About";
+import Error from './pages/Error';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Housing from "./pages/Housing"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="about" element={<About/>}/>
+        <Route path="housing/:id/*" element={<Housing/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<Error/>} />
+      </Routes>
+    <Footer/>
+    </Router> 
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
